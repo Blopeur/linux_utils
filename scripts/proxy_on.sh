1d461b28efad3f6e794bc0e98e2bd8e2ee8450c7
Benoit Hudzia <benoit.hudzia@gmail.com>
2013-09-06 08:25:28 +0100

    adding bug needed to be fixed

--- scripts/proxy_on.sh
+++ scripts/proxy_on.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export http_proxy=http://proxy.dub.sap.corp:8080
-export https_proxy=http://proxy.dub.sap.corp:8080
+export http_proxy="http://proxy.dub.sap.corp:8080"
+export https_proxy="http://proxy.dub.sap.corp:8080"
